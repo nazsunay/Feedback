7d9baea80a66ce294571d950d5792acacd619e2a
neslihannur <hanneslihan34@gmail.com>
2024-10-27 10:54:11 +0300

    Merhaba

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,6 +13,7 @@ namespace Feedback.Controllers
 
         private readonly ILogger<WeatherForecastController> _logger;
 
+
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
